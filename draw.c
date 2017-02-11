2c865ba87ccc0b83c4addbb7b8f3b0a1fd4d9d4d
FelixRiegBaumhauer <friegbaumhauer@stuy.edu>
2017-02-10 19:43:20 -0500

    Line algo done

--- draw.c
+++ draw.c
@@ -7,5 +7,34 @@
 
 //Insert your line algorithm here
 void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
+    
+  //needed variables
+  int A;
+  int B;
+
+  int x = x0;
+  int y = y0;
+
+  //setup
+  A = y1-y0;
+  B = (-1) * (x1-x0);
+  //for now we can subtract x1 from x0, as we ae dealing with octet 1
+
+  int d = 2*A + B;
+
+  A=2*A;
+  B=2*B;
+  
+  while(x<=x1){
+    plot(s, c, x, y);
+
+    if(d>0){
+      y++;
+      d+=B;
+    }
+    x++;
+    d+=A;
+  }
+  
 }
 
--- main.c
+++ main.c
@@ -18,7 +18,19 @@ int main() {
   
   clear_screen(s);
 
+  c.red = MAX_COLOR;
+  c.green = MAX_COLOR;
+  c.blue = MAX_COLOR;
+  draw_line(1, 2, 278, 146, s, c);
 
+  c.red = 120;
+  c.green = MAX_COLOR;
+  c.blue = 0;
+  draw_line(56, 89, 432, 108, s, c);
+
+
+  
   display(s);
+  save_ppm(s, "lines.ppm");
   save_extension(s, "lines.png");
 }  
