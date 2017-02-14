9431f6f0328fb531a495e762985f2d89643040c1
FelixRiegBaumhauer <friegbaumhauer@stuy.edu>
2017-02-14 18:46:40 -0500

    easy version done

--- draw.c
+++ draw.c
@@ -5,6 +5,7 @@
 #include "display.h"
 #include "draw.h"
 
+/*
 //Insert your line algorithm here
 void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
     
@@ -38,9 +39,7 @@ void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
   
 }
 
-
-
-//////
+//222
 
 void draw_line_2(int y0, int x0, int y1, int x1, screen s, color c) {
     
@@ -74,3 +73,164 @@ void draw_line_2(int y0, int x0, int y1, int x1, screen s, color c) {
   
 }
 
+*/
+
+
+//////BAD METHOD:
+void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
+  //[3-6]
+  if(x0>x1){
+    draw_line(x1, y1, x0, y0, s, c);
+  }
+  //else you in oct 1,2,7,8
+  else{
+    //7,8
+    if(y0>y1){
+      if(x1-x0 > y0-y1){//0
+	draw_line_8(x0, y0, x1, y1, s, c);
+      }
+      else{
+	draw_line_7(x0, y0, x1, y1, s, c);
+      }
+    }
+    //1,2
+    else{
+      if(x1-x0 > y1-y0){//0
+	draw_line_1(x0, y0, x1, y1, s, c);
+      }
+      else{
+	draw_line_2(x0, y0, x1, y1, s, c);
+      }
+    }
+  }
+}
+
+//Insert your line algorithm here
+void draw_line_1(int x0, int y0, int x1, int y1, screen s, color c) {
+    
+  //needed variables
+  int A;
+  int B;
+
+  int x = x0;
+  int y = y0;
+
+  //setup
+  A = y1-y0;
+  B = (-1) * (x1-x0);
+  //for now we can subtract x1 from x0, as we ae dealing with octet 1
+
+  int d = 2*A + B;
+
+  A=2*A;
+  B=2*B;
+  
+  while(x<=x1){
+    plot(s, c, x, y);
+
+    if(d>0){
+      y++;
+      d+=B;
+    }
+    x++;
+    d+=A;
+  }
+}
+
+//Insert your line algorithm here
+void draw_line_2(int x0, int y0, int x1, int y1, screen s, color c) {   
+  //needed variables
+  int A;
+  int B;
+
+  int x = x0;
+  int y = y0;
+
+  //setup
+  A = y1-y0;
+  B = (-1) * (x1-x0);
+  //for now we can subtract x1 from x0, as we ae dealing with octet 1
+
+  int d = A + 2*B;
+
+  A=2*A;
+  B=2*B;
+  
+  while(y<=y1){
+    plot(s, c, x, y);
+
+    if(d<0){
+      x++;
+      d+=A;
+    }
+    y++;
+    d+=B;
+  }
+}
+
+
+//Insert your line algorithm here
+void draw_line_7(int x0, int y0, int x1, int y1, screen s, color c) {
+    
+  //needed variables
+  int A;
+  int B;
+
+  int x = x0;
+  int y = y0;
+
+  //setup
+  A = y1-y0;
+  B = (-1) * (x1-x0);
+  //for now we can subtract x1 from x0, as we ae dealing with octet 1
+
+  int d = A - 2*B;
+
+  A=2*A;
+  B=2*B;
+  
+  while(y>=y1){
+    plot(s, c, x, y);
+
+    if(d>0){
+      x++;
+      d+=A;
+    }
+    y--;
+    d-=B;
+  }
+}
+
+
+//Insert your line algorithm here
+void draw_line_8(int x0, int y0, int x1, int y1, screen s, color c) {
+  
+  //needed variables
+  int A;
+  int B;
+
+  int x = x0;
+  int y = y0;
+
+  //setup
+  A = y1-y0;
+  B = (-1) * (x1-x0);
+  //for now we can subtract x1 from x0, as we ae dealing with octet 1
+
+  int d = 2*A - B;
+
+  A=2*A;
+  B=2*B;
+  
+  while(x<=x1){
+    plot(s, c, x, y);
+
+    if(d<0){
+      y--;
+      d-=B;
+    }
+    x++;
+    d+=A;
+  }
+  
+}
--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 #include "display.h"
 #include "draw.h"
 
+
 int main() {
 
   screen s;
@@ -21,12 +22,39 @@ int main() {
   c.red = MAX_COLOR;
   c.green = MAX_COLOR;
   c.blue = MAX_COLOR;
+  
+  int i=0;
+  for(i=0; i<=10; i++){
+    draw_line(250,250,50*i, 500,s,c);
+  }
+  
+  for(i=0; i<=10; i++){
+    draw_line(250,250,50*i, 0,s,c);
+  }
+
+  for(i=0; i<=10; i++){
+    draw_line(250,250,500,50*i,s,c);
+  }
+
+  for(i=0; i<=10; i++){
+    draw_line(250,250,0,50*i,s,c);
+  }
+  
+  
+  
+  /*
   draw_line(1, 2, 278, 146, s, c);
 
   draw_line(56, 89, 432, 108, s, c);
 
   draw_line_2(0, 0, 100, 300, s, c);
-
+  */
+  /*
+  draw_line_1(10,10,500,100,s,c);
+  draw_line_2(10,10,100,500,s,c);
+  draw_line_7(10,490,100,100,s,c);
+  draw_line_8(10,490,500,400,s,c);
+  */
   //draw_line(100, 500, 400, 400, s, c);
 
 
